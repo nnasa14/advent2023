4aab9d2439edf11deb2bb3004fe0d260ee7379d0
nnasa14 <nnascimento.kr@gmail.com>
2023-12-02 20:22:08 -0500

    corrected  solution, part one of day one complete

--- trebuchetDayOne.py
+++ trebuchetDayOne.py
@@ -5,32 +5,13 @@
 # append two-digit num into a sum
 # return sum
 
-'''def trebuchet(arr):
-    sum = 0
-    digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-
-    for i in arr:
-        nums = []
-        for chars in i:
-            if chars in digits:
-                nums.append(chars)
-        first = nums[0]
-        last = nums[-1]
-        double_digits = str(first) + str(last)
-        sum += int(double_digits)
-
-    return sum
-
-arr = ['1abc2', 'pqr3stu8vwx', 'a1b2c3d4e5f', 'treb7uchet'] # output 142
-print(trebuchet(arr))'''
+import re
 
 sum = 0
-import re
 with open('trebuchetPuzzleInput.txt', 'r') as file:
     puzzle_input = file.read().split('\n')
 
 for line in puzzle_input:
-    digits = re.findall(r'\d+', line)
+    digits = re.findall(r'\d', line)
     sum += int(digits[0] + digits[-1])
-
 print(sum)
